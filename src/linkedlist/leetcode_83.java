dcddd6bde03af4eeae9a04c301dfec5ed82abd2d
OEG <it.nhanpd@gmail.com>
2026-09-08 17:23:46 +0700

    add: linkedlist - leetcode_83

--- src/linkedlist/leetcode_83.java
+++ src/linkedlist/leetcode_83.java
@@ -1,24 +1,35 @@
 package linkedlist;
 
 import common.ListNode;
-import java.util.HashSet;
-import java.util.Set;
 
 public class leetcode_83 {
+    // public static ListNode deleteDuplicates(ListNode head) {
+    //     ListNode dummy = new ListNode(-1);
+    //     ListNode tail = dummy;
+    //     Set<Integer> set = new HashSet<>();
+    //     while(head != null){
+    //         if(!set.contains(head.val)){
+    //             set.add(head.val);
+    //             tail.next = head;
+    //             tail = tail.next;
+    //         }
+    //         head = head.next;
+    //     }
+    //     tail.next = null;
+    //     return dummy.next;
+    // }
+
     public static ListNode deleteDuplicates(ListNode head) {
-        ListNode dummy = new ListNode(-1);
-        ListNode tail = dummy;
-        Set<Integer> set = new HashSet<>();
-        while(head != null){
-            if(!set.contains(head.val)){
-                set.add(head.val);
-                tail.next = head;
-                tail = tail.next;
+        ListNode curr = head;
+        while(curr != null && curr.next != null){
+            if(curr.val != curr.next.val){
+                curr = curr.next;  
+            }else{
+                curr.next = curr.next.next;
+                
             }
-            head = head.next;
         }
-        tail.next = null;
-        return dummy.next;
+        return head;
     }
 
     public static void main(String[] args){
